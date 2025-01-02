eafa9d72f2c42d0a3abfa419ec83d9be634eabcb
Jeremie Litzler <jlitzler@cbtw.tech>
2025-01-02 14:13:44 +0100

    add minimun pages with Unplugin Vue Router

---
+++ src/pages/demo/[slug].vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Demo page - Slug {{ slug }}</h1>
+
+</template>
+
+<script setup lang="ts">
+const slug = ref('')
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/demo/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Demo page - Index</h1>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
