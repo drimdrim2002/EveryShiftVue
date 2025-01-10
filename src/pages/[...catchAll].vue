f520e85c5593dd7f4506b31a8a715b6004793f4a
Jeremie Litzler <jlitzler@cbtw.tech>
2025-01-10 14:57:02 +0100

    add 404 page

---
+++ src/pages/[...catchAll].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AppError from '@/components/AppError.vue'
+
+useErrorStore().setError({ error: 'Page Not Found', customCode: 404 })
+</script>
+
+<template>
+  <AppError />
+</template>
