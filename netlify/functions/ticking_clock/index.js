a7978f086fece10780def6ddf9d3482b7601744e
Jeremie Litzler <jlitzler@cbtw.tech>
2025-01-21 15:16:54 +0100

    feat: run clock function every hour

--- netlify/functions/ticking_clock/index.js
+++ netlify/functions/ticking_clock/index.js
@@ -1,5 +1,5 @@
 const { schedule } = require('@netlify/functions')
 
-exports.handler = schedule('* */1 * * *', async () => {
+exports.handler = schedule('0 * * * *', async () => {
   console.log(new Date(Date.now()))
 })
